4ba723706e482c7c41929a552be34d7d53ebe0aa
Fazle Rabbi Ador <ador@thedoodleinc.com>
2022-08-08 18:27:42 +0600

    model and routes added, db config change

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
+var cors = require('cors');
 const connectDB = require('./config/db')
+const books = require('./routes/api/books')
 
-const app = express()
+const app = express();
 
 // connect to database
 connectDB()
 
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
 app.get('/', (req, res) => res.send('Hello world from node!'))
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082
 
